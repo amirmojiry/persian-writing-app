d831bd3f46734edc4eb6f9ab5b598ec0c132c6d0
Amirhossein Mojiri Foroushani <amojiry@gmail.com>
2026-08-03 00:28:06 +0300

    Add cloud administrator role

--- apps/api/app/Models/User.php
+++ apps/api/app/Models/User.php
@@ -8,11 +8,17 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
+/** @property string $email @property bool $is_admin */
 final class User extends Authenticatable
 {
     use HasApiTokens;
     use HasUuids;
 
-    protected $fillable = ['email'];
+    protected $fillable = ['email', 'is_admin'];
     protected $hidden = [];
+
+    protected function casts(): array
+    {
+        return ['is_admin' => 'boolean'];
+    }
 }
