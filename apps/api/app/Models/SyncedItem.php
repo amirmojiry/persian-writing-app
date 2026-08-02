53102165ca5012bc35a47faac6f60effcb8157cb
Amirhossein Mojiri Foroushani <amojiry@gmail.com>
2026-08-03 00:39:32 +0300

    Describe synchronized item casts precisely

--- apps/api/app/Models/SyncedItem.php
+++ apps/api/app/Models/SyncedItem.php
@@ -7,6 +7,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $user_id
+ * @property string $idempotency_key
+ * @property string $aggregate_type
+ * @property string $aggregate_id
+ * @property string $operation
+ * @property array<string, mixed> $payload
+ * @property array<string, mixed> $consent_snapshot
+ */
 final class SyncedItem extends Model
 {
     use HasUuids;
