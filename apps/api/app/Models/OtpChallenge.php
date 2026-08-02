c3c7d7b2bc702b4247a0940274d9d87a8004f783
Amirhossein Mojiri Foroushani <amojiry@gmail.com>
2026-08-02 14:41:31 +0300

    Document OTP model cast types

--- apps/api/app/Models/OtpChallenge.php
+++ apps/api/app/Models/OtpChallenge.php
@@ -4,9 +4,17 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $normalized_email
+ * @property string $code_hash
+ * @property CarbonImmutable $expires_at
+ * @property int $attempts
+ * @property CarbonImmutable|null $consumed_at
+ */
 final class OtpChallenge extends Model
 {
     use HasUuids;
